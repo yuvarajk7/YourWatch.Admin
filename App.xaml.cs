6342f3e3c5e5b2818f62e7d762acb75e64ddfcae
Yuvaraj <yuvaraj.kesavan@hotmail.com>
2024-04-26 22:48:01 -0500

    Create MAUI YourWatch Admin Mobile Project

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace YourWatch.Admin.Mobile;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace YourWatch.Admin.Mobile;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
